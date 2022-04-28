4da4cc239a0f8f2399077cc3cbafd444c3937294
Nicholas Meade <nicholasjmeade@hotmail.com>
2022-04-28 15:13:30 -0400

    Most responsive views complete 1. rewrote navbar for footer and looks much better 2. going to ditch hamburger icon for now, I think the simplest thing I can do to incorporate JS for sake of time is a weather API (which was something I wanted to do anyway in my wireframe drawing) 3. will focus on JS component now which will satisfy all MVP when completed

---
+++ portfolio_website_contents/main.js
@@ -0,0 +1,7 @@
+// const hamburger = document.querySelector(".hamburger");
+// const navBar = document.querySelector(".navBar");
+
+// hamburger.addEventListener("click", () => {
+//     hamburger.classList.toggle("active");
+//     navBar.classList.toggle("active");
+// })
