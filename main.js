e04412b736b9919f27400ddcd572cd7e64c8082d
Nicholas Meade <nicholasjmeade@hotmail.com>
2022-06-23 15:45:27 -0400

    removed weather api

--- main.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-    method: "get",
-    url: "https://api.openweathermap.org/data/2.5/weather?lat=40.7128&lon=-74.0060&appid=ecbafc8bc682243cadc5b7330751bcef&units=imperial"
-})
-
-    .then(function(response){
-        console.log(response)
-        $('.aboutMe').append(`<p class="temperature"><a href="https://www.weather.gov/" target="blank">My current weather is ${response.main.temp}°. Click here to find out yours!</a></p>`)
-    })
